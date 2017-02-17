d43ac8cbaf53e72bab0a26e46493884efe114b69
Andrew <andrew@lootinteractive.com>
2017-02-16 17:06:39 -0800

    test

--- index.js
+++ index.js
@@ -68,8 +68,8 @@ controller.setupWebserver(env.PORT, function (err, webserver) {
     controller.createWebhookEndpoints(controller.webserver);
 });
 
-var services = [Object.create(require('./lib/loottool/BlockifierService')),
-				Object.create(require('./lib/loottool/GachaService'))];
+var services = [new require('./lib/loottool/BlockifierService'),
+				new require('./lib/loottool/GachaService')];
 
 //
 // BEGIN EDITING HERE!
